a139d6d615b9643fa08eeb65332383a292a184d1
zld <zldproductions343@gmail.com>
2017-06-29 08:24:17 -0700

    Updated Help Contents. Added h$prefix command.

--- help.py
+++ help.py
@@ -5,7 +5,6 @@ from datetime import datetime
 import discord
 from colorama import Fore, init
 
-# noinspection SpellCheckingInspection
 token = 'MjU1Mzc5NzQ4ODI4NjEwNTYx.CycwfQ.c6n0jvVrV5lGbbke68dHdlYMRX0'
 client = discord.Client()
 init()
