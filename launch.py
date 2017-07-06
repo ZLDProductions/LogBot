4ced25936b39405b97e9c72072c20ae9d5b94994
zld <zldproductions343@gmail.com>
2017-07-05 20:01:58 -0700

    Small improvements made to launch.py

--- launch.py
+++ launch.py
@@ -1,4 +1,8 @@
 import os
+import sqlite3
+
+sql = sqlite3.connect("launch.db")
+cursor = sql.cursor()
 
 my_str = """0. logbot.py
 1. bible.py
@@ -35,12 +39,65 @@ titles = {
 	"9":"Swearing Filter V2 Plugin"
 }
 
+# <editor-fold desc="Standard initialization">
+try: cursor.execute(f"""
+CREATE TABLE packages (name VARCHAR(50), sequence VARCHAR(50));
+""".replace("\t", ""))
+except: pass
+try: cursor.execute(f"""
+CREATE INDEX pkg_index
+ON packages (name, sequence);
+""".replace("\t",""))
+except: pass
+# </editor-fold>
+
+def getRunPackages():
+	cursor.execute(f"""
+	SELECT *
+	FROM packages;
+	""".replace("\t", ""))
+	res = cursor.fetchall()
+	for pkg in res: print(f"{res.index(pkg)}. {pkg[0]} - {pkg[1]}")
+	seq = res[int(input("Sequence to run: "))][1]
+	for c in seq: os.system(f"start \"{titles[c.lower()]}\" /MAX /HIGH python {os.getcwd()}\\{programs[c.lower()]}")
+	pass
+
 def main():
-	print(my_str)
-	files = input("Programs to launch: ")
-	for c in files:
-		os.system(f"start \"{titles[c.lower()]}\" /MAX /HIGH python {os.getcwd()}\\{programs[c.lower()]}")
+	print("1. Run sequence\n2. Run package\n3. Create package")
+	choice = int(input("Operation: "))
+	if choice == 1:
+		print(my_str)
+		files = input("Programs to launch: ")
+		for c in files:
+			os.system(f"start \"{titles[c.lower()]}\" /MAX /HIGH python {os.getcwd()}\\{programs[c.lower()]}")
+			pass
+		pass
+	elif choice == 2:
+		getRunPackages()
+		pass
+	elif choice == 3:
+		name = input("Package name: ")
+		sequence = input("Package sequence: ")
+		cursor.execute(f"""
+		SELECT *
+		FROM packages
+		WHERE name="{name}"
+		OR sequence="{sequence}";
+		""".replace("\t", ""))
+		res = cursor.fetchall()
+		if len(res) >= 1:
+			print("That package already exists.")
+			pass
+		else:
+			cursor.execute(f"""
+			INSERT INTO packages (name, sequence)
+			VALUES ("{name}", "{sequence}");
+			""".replace("\t", ""))
+			print("Created the package.")
+			pass
+		main()
 		pass
 	pass
 
 main()
+sql.commit()
